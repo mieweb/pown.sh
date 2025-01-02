ef1fa0ed94bb22e7b1bba1b7c8b6f6fb7b8cfe23
anishapant21 <anishapant1@gmail.com>
2025-01-02 10:58:36 -0500

    Refacotor terraform.yml

--- tests/main.tf
+++ tests/main.tf
@@ -32,88 +32,17 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-# Fetch the latest Debian AMI
-data "aws_ami" "debian" {
-  most_recent = true
-  owners      = ["136693071363"] # Owner ID for Debian
-  filter {
-    name   = "name"
-    values = ["debian-12-amd64-*"]
-  }
-}
-
-# Fetch the latest Amazon Linux AMI
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["137112412989"] # Owner ID for Amazon Linux
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*-x86_64"]
-  }
-}
-
-resource "aws_instance" "debian" {
-  ami             = data.aws_ami.debian.id
-  instance_type   = "t2.micro"
+resource "aws_instance" "test" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
   security_groups = [aws_security_group.allow_ssh.name]
-  key_name        = "test"
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo 'Step 1: Creating .env file'
-              cat << 'ENVFILE' > /root/.env
-              $(cat .env)
-              ENVFILE
-              
-              chmod 600 /root/.env
-              
-              echo 'Step 2: Creating pown script'
-              cat << 'SCRIPT' > /root/pown.sh
-              $(cat ../pown.sh)
-              SCRIPT
-              
-              chmod +x /root/pown.sh
-              bash /root/pown.sh
-              EOF
+  key_name      = "test"
 
   tags = {
-    Name = "DebianInstance"
+    Name = "TestInstance"
   }
 }
 
-resource "aws_instance" "amazon_linux" {
-  ami             = data.aws_ami.amazon_linux.id
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.allow_ssh.name]
-  key_name        = "test"
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo 'Step 1: Creating .env file'
-              cat << 'ENVFILE' > /root/.env
-              $(cat .env)
-              ENVFILE
-              
-              chmod 600 /root/.env
-              
-              echo 'Step 2: Creating pown script'
-              cat << 'SCRIPT' > /root/pown.sh
-              $(cat ../pown.sh)
-              SCRIPT
-              
-              chmod +x /root/pown.sh
-              bash /root/pown.sh
-              EOF
-
-  tags = {
-    Name = "AmazonLinuxInstance"
-  }
-}
-
-output "debian_public_ip" {
-  value = aws_instance.debian.public_ip
-}
-
-output "amazon_linux_public_ip" {
-  value = aws_instance.amazon_linux.public_ip
+output "instance_ip" {
+  value = aws_instance.test.public_ip
 }
